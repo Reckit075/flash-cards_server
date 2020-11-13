f0ce65f02da85763c486dbe39ba3cf4cf9f8f91b
Reckit075 <mateusz.majka.zsl@gmail.com>
2020-11-13 18:41:25 +0100

    fix gitignore

--- secret/gmail_data.mjs
+++
@@ -1,4 +0,0 @@
-const emailPassword = 'TestTest1'
-const emailLogin = 'testtest8395@gmail.com'
-
-export {emailPassword,emailLogin}
--- secret/keys.mjs
+++
@@ -1,3 +0,0 @@
-export default {
-    key: "Zr4t7w!z%C*F-JaNdRgUkXp2s5v8x/A?"
-}
